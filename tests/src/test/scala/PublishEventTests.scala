ff69e082c7ac85212914027fabe6053b45546a22
Amit M Mangalvedkar <amitmangalvedkar@in.ibm.com>
2017-07-18 16:51:35 +0530

    Added test case for publishing events considering a pre-existing IoT service

--- tests/src/test/scala/PublishEventTests.scala
+++ tests/src/test/scala/PublishEventTests.scala
@@ -1,2 +1,48 @@
 
 
+package packages
+
+import common.{TestHelpers, Wsk, WskProps, WskTestHelpers, _}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import spray.json.DefaultJsonProtocol.StringJsonFormat
+import spray.json.pimpAny
+
+@RunWith(classOf[JUnitRunner])
+class PublishEventTests
+    extends TestHelpers
+    with WskTestHelpers{
+  implicit val wskprops = WskProps()
+  val wsk = new Wsk()
+
+  // Retrieving all VCAP credentials, although currently only a few would be used
+  val credentials = TestUtils.getVCAPcredentials("iotf-service");
+  val iotCredentialsIdentifier = credentials.get("iotCredentialsIdentifier");
+  val mqttHost = credentials.get("mqtt_host");
+  val mqttUPort = credentials.get("mqtt_u_port");
+  val mqttSPort = credentials.get("mqtt_s_port");
+  val httpHost = credentials.get("http_host");
+  val org = credentials.get("org");
+  val apiKey = credentials.get("apiKey");
+  val apiToken = credentials.get("apiToken");
+
+  //These values need to be passed
+  val gatewayTypeId = "gatewayType";
+  val gatewayId = "gatewayId";
+  val gatewayToken = "password";
+
+  val typeId = "deviceType";
+  val deviceId = "deviceId";
+  val eventType = "evt";
+  val payload = ("d" -> ("temp" -> 25));
+
+
+  behavior of "WIoTP Package"
+
+    it should "Publish Event action" in {
+           val name = "/whisk.system/watsoniotplatform/publishEvent"
+             withActivation(wsk.activation,wsk.action.invoke(name, Map("org" -> org, "domain" -> mqttHost, "gatewayTypeId" -> gatewayTypeId, "gatewayId" -> gatewayId, "gatewayToken" -> gatewayToken, "typeId" -> typeId, "deviceId" -> deviceId, "eventType" -> eventType, "payload" -> payload))){
+                 _.response.result.get.toString should include ("body")
+             }
+    }
+}
