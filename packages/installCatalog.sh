e683b895305e33e057cb6a11cc9d1dceaa813cee
Carlos Santana <csantana@us.ibm.com>
2017-07-19 15:04:11 -0400

    fix annotation for orgId

--- packages/installCatalog.sh
+++ packages/installCatalog.sh
@@ -39,7 +39,7 @@ echo Installing Watson IoT platform package.
 
 $WSK_CLI -i --apihost "$APIHOST"  package update --auth "$AUTH"  --shared yes ${PACKAGE_NAME} \
 -a description "This is the package for Watson IoT Platform." \
--a parameters '[{"name":"org", "required":true, "bindTime":true, "description":"Organization ID"},
+-a parameters '[{"name":"orgId", "required":true, "bindTime":true, "description":"Organization ID"},
 {"name":"gatewayToken", "required":true, "bindTime":true, "type":"password", "description":"Token for the Gateway Registered in the platform"},
 {"name":"domain", "required":false, "bindTime":true, "description":"Domain of the messaging server"},
 {"name":"gatewayTypeId", "required":true, "bindTime":true, "description":"Gateway Type ID"},
@@ -49,7 +49,7 @@ $WSK_CLI -i --apihost "$APIHOST"  package update --auth "$AUTH"  --shared yes ${
 
 $WSK_CLI -i --apihost "$APIHOST" action update --auth "$AUTH" "${PACKAGE_NAME}/publishEvent" "$PACKAGE_HOME/actions/publishEvent.js" \
 -a description 'Send event to the Watson IoT Plaform as a IoT Gateway' \
--a parameters '[ {"name":"org", "required":true, "bindTime":true, "description":"Organization ID"},
+-a parameters '[ {"name":"orgId", "required":true, "bindTime":true, "description":"Organization ID"},
 {"name":"gatewayToken", "required":true, "bindTime":true, "type":"password", "description":"Token for the Gateway Registered in the platform"},
 {"name":"domain", "required":false, "bindTime":true, "description":"Domain of the messaging server"},
 {"name":"gatewayTypeId", "required":true, "bindTime":true, "description":"Gateway Type ID"},
@@ -58,5 +58,5 @@ $WSK_CLI -i --apihost "$APIHOST" action update --auth "$AUTH" "${PACKAGE_NAME}/p
 {"name":"typeId", "required":true, "description":"Type ID of the device attached"},
 {"name":"deviceId", "required":true, "description":"Device ID of the device attached"},
 {"name":"payload", "required":true, "description":"Payload of the device event"}]' \
--a sampleInput '{"deviceId":"xxx-xxx-xx", "typeId":"yyy-yyy-yyy", "payload":"hi there", "docId" : "xxxx-xxxx-xxxx"}' \
+-a sampleInput '{"deviceId":"xxx-xxx-xx", "typeId":"yyy-yyy-yyy", "payload":"hi there"}' \
 -a sampleOutput '{}'
